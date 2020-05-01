9f344715d9fee3bf8992804dbc8edfc6085d8b24
Mukul Dharwadkar <mukul.dharwadkar@gmail.com>
2020-04-30 21:54:12 -0700

    Sort any file alphabetically and add alphabetical index

--- sort_file.py
+++ sort_file.py
@@ -2,15 +2,17 @@
 # Progammer: Mukul Dharwadkar
 # Date: 29 April 2020
 # Python Version 3
-# Program version 1
+# Program version 1: Basic program with sort and hardcoded output file name
+# Program version 2: Basic program name with sort and output file name as an argument
 
 import sys
 
 def sort_file():
     with open(sys.argv[1], "r") as file1:
-        newFile = "sortedsongs.txt"
+        newFile = sys.argv[2]
         nf = open(newFile, "w+")
         for line in sorted(file1):
             nf.write(line)
+        nf.close()
 
 sort_file()
