fc1dada515cc1b698939e3b779b276929577edf9
Mukul Dharwadkar <mukul.dharwadkar@gmail.com>
2020-05-09 21:39:50 -0700

    Removing old files

--- resolve_conflict.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-import subprocess
-
-#path = "/Users/dharmuku/Documents/Personal/src/python/"
-file = open("merge_conflict.txt", "r")
-for line in file:
-    subprocess.run(["git", "add", line])
-    #print(line)
